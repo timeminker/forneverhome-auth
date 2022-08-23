51f73bffebdd7c9965dddbe98655d2ed009d18fc
tim minker <timminker@gmail.com>
2022-08-23 17:46:26 -0400

    about page transition effect added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="about">
     <h1>For-Never Home</h1><br/>
-    <h3>Gone but not forgotten, For-Never Home is dedicated to the furry/scaly, big/small, wet/dry friends that we have shared our hearts and lives with. Let your pal live on into the digital age; clouds don't only exist in heaven.</h3>
+    <div class="background">
+      <h3>Gone but not forgotten, For-Never Home is dedicated to the furry/scaly, big/small, wet/dry friends that we have shared our hearts and lives with. Let your pal live on into the digital age; clouds don't only exist in heaven.</h3>
+    </div>
     <button @click="$router.push('/')">Return home</button>
   </div>
 </template>
@@ -16,7 +18,18 @@
     width: 1000px;
     margin-left: auto;
     margin-right: auto;
+  }
+  .background {
+    width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
     background: rgb(10, 10, 10, .50);
     border-radius: 15px 15px 15px 15px;
+    transition: width 2s;
+    text-align: center;
+  }
+  .background:hover {
+    width: 1200px;
+    text-align: center;
   }
 </style>
